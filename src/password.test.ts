9e05aeb112f10b74754d734a89feaf086dafb191
johnlim0218 <gwanwoo.john@gmail.com>
2022-04-12 00:13:52 +0900

    테스트 케이스 추가

--- src/password.test.ts
+++ src/password.test.ts
@@ -1,10 +1,8 @@
-import { exceptionTest, passwordMeter } from "./password";
+import { exceptionTest, passwordMeter, PasswordStrength } from "./password";
 
 describe('암호검사기', () => {
   test('test(message)', async () => {
-    
     // expect(nullInput('1')).toBe('2');
-
     expect(exceptionTest).toThrowError();
   });
 
@@ -14,9 +12,51 @@ describe('암호검사기', () => {
     }).toThrowError();
   });
 
-  test('빈 값 입력이면 익셉션 발생', () => {
+  test('빈 값 입력이면 익셉션 발생(undefined)', () => {
     expect(() => {
       passwordMeter(undefined)
     }).toThrowError();
   });
+
+  test('빈 값 입력이면 익셉션 발생("")', () => {
+    expect(() => {
+      passwordMeter('')
+    }).toThrowError();
+  });
+
+  test('모든 조건을 충족하면 강함', () => {
+    expect(
+      passwordMeter('abcABC123')
+    ).toBe(PasswordStrength.STRONG);
+    expect(
+      passwordMeter('123abcABC')
+    ).toBe(PasswordStrength.STRONG);
+  });
+
+  test('길이가 8미만, 다른 조건 충족', () => {
+    expect(
+      passwordMeter('abcC123')
+    ).toBe(PasswordStrength.NORMAL);
+    expect(
+      passwordMeter('123abcC')
+    ).toBe(PasswordStrength.NORMAL);
+    expect(
+      passwordMeter('Cabc12')
+    ).toBe(PasswordStrength.NORMAL);
+  });
+
+  test('대문자 없음, 다른 조건 충족', () => {
+    expect(
+      passwordMeter('abcd1234')
+    ).toBe(PasswordStrength.NORMAL);
+  });
+
+  test('숫자 없음, 다른 조건 충족', () => {
+    expect(
+      passwordMeter('ABCDabcde')
+    ).toBe(PasswordStrength.NORMAL);
+    expect(
+      passwordMeter('abcdeABCD')
+    ).toBe(PasswordStrength.NORMAL);
+  })
 })
--- src/password.ts
+++ src/password.ts
@@ -1,10 +1,23 @@
+export enum PasswordStrength {
+  STRONG = 'strong',
+  NORMAL = 'normal'
+}
+
 export function exceptionTest () {
   throw new Error('error');
 }
 
 export function passwordMeter(password?: string | null) {
-  if (!password) {
-    throw new Error('error');
-  }
-  return password;
+  if (!password || password === '') throw new Error('error');
+  // if (password === 'abcC123' || password === '123abcC') return PasswordStrength.NORMAL;
+  let lengthRull = password.length >= 8;
+  if (!lengthRull) return PasswordStrength.NORMAL;
+  
+  let foundUppercase = /[A-Z]/.test(password);
+  if (!foundUppercase) return PasswordStrength.NORMAL;
+
+  let foundNumber = /[0-9]/.test(password);
+  if (!foundNumber) return PasswordStrength.NORMAL;
+
+  return PasswordStrength.STRONG;
 }
